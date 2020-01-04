2d732b9d907d7b28fbaccf3c204f32898f5c6b78
FreakyHarsh <harsh.boricha2015@gmail.com>
2020-01-04 19:44:28 +0530

    Operator overloading in python

---
+++ Operator_Overloading.py
@@ -0,0 +1,16 @@
+# This Program shows how * operator is used to add two numbers
+# instead of multiplying them
+class Number:
+    def __init__(self, x):
+        self.x = x
+
+    def __mul__(self, other):
+        x = self.x + other.x
+        return x
+
+
+number_a = Number(3)
+
+number_b = Number(4)
+
+print(number_a * number_b)
